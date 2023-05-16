e7d0d874f08b92a0e742bff49ab2fb49f8e84b7b
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2023-05-16 13:31:51 +0200

    avuii

--- Assets/powerUpProjecte.cs
+++ Assets/powerUpProjecte.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class powerUpProjecte : MonoBehaviour
 {
     //dins es game manager
     public bool appleGreenIsOn = false;
-    public GameObject sliderPanelGreen; 
+    public GameObject sliderPanelGreen;
+    public float time; //(una variable que ja està feta dins s'escript de power ups)
     /* 
          - conexió amb sa speed (PlayerMovement.walkingForce)
          - 
@@ -18,11 +20,11 @@ public class powerUpProjecte : MonoBehaviour
         if(other.gameObject.CompareTag("appleGreen") && appleGreenIsOn == false)
         {
             Destroy(other.gameObject);
-
+            StartCoroutine("SpeedPowerup(walkingForce)");
         }
     }
 
-    //això anirà ficat dins;   --> canviar d'on agafa sa variable, l'ha de agafar de script "PoweUp"
+    //això anirà ficat dins;   --> canviar d'on agafa sa variable, l'ha de agafar de script "PowerUp"
     /* if(other.CompareTag(appleGreen) && gameManagerScript.appleGreenIsOn == false){ */
     public IEnumerator SpeedPowerUp(float speed,float durationOfPowerUp)
    {
@@ -30,9 +32,39 @@ public class powerUpProjecte : MonoBehaviour
         appleGreenIsOn = true;
         sliderPanelGreen.SetActive(true);
 
+        time = durationOfPowerUp;
+        StartCoroutine("Counter"); //--> ja està dins s'escript de power up
+
         yield return new WaitForSeconds(durationOfPowerUp);
 
         appleGreenIsOn = false;
    }
+    private IEnumerator Counter(Slider slider, GameObject panelOfTheSlider, int numOfPowerUp)
+    {   //it displays the seconds 
 
+        slider.maxValue = time;//posam que es valor màxim de slider sigui es temps que ha d'esperar
+
+        while (time > 0)
+        {
+            time -= 0.5f;
+            slider.value = time;
+
+            //as soon as the time is over, restablish the values
+            if (time == 0)
+            {
+                panelOfTheSlider.SetActive(false);
+                if(numOfPowerUp == 1)
+                {
+                    playerMovementScript.Scale(1.5f);
+                }
+                if(numOfPowerUp == 2)
+                {
+                    playerMovementScript.walkingForce = 0.5f; //restablish the speed
+                }
+
+            }
+            yield return new WaitForSeconds(0.5f); //wait 30 seconds to low the number
+        }
+
+    }
 }
